86bd218e820a9d94ced6158252e6d9625b3e7e75
Underlord711 <wchmar711@gmail.com>
2024-04-11 09:53:53 -0400

    Practiced Implementing components onto a page

---
+++ site/Components/home.jsx
@@ -0,0 +1,9 @@
+
+
+export const Home = () => {
+    return (
+        <div>
+            <h1>Hello</h1>
+        </div>
+    );
+};
--- site/app/layout.js
+++ site/app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Header } from "../Components/header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,7 +12,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <Header/>
+      <body className={inter.className}>
+        {children}
+        </body>
     </html>
   );
 }
