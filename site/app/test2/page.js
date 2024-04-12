a9537c774387828973f4ae20c9d4bed8c776bd15
Spencer Presley <spencerpresley96@gmail.com>
2024-04-12 15:52:39 -0400

    figured out how to load data from json, only partly done, still need to load more as it just came from a test file

--- site/app/test2/page.js
+++ site/app/test2/page.js
@@ -1,47 +1,35 @@
 import { Themes } from "@/components/Research"
 import { CatCard } from "@/components/catInfoCard"
-import { CatAccord } from "@/components/catInfoAccord"
-import fsPrommises from 'fs/promises';
-import path from 'path';
+import { CatAccord } from '@/components/catInfoAccord';
 
-
-export async function getStaticProps() {
-    // Path to the JSON file
-    const filePath = path.join(process.cwd(), 'public/test2.json');
-    
-    // Read the JSON file
-    const jsonData =await fsPrommises.readFile(filePath)
-    const data = JSON.parse(jsonData);
+import { useCatData } from "@/components/useCatData.server";
   
-    // Pass the items array to the page via props
-    return objectData
-}
+export default function page() {
+    const data = useCatData();
 
-  
-export default function page({items}) {
-  return (
-    <div>
-        <div className="grid grid-rows-5 grid-cols-3 gap-4 grid-flow-row-dense pl-20 pr-20">
-            <div className="bg-red-500 text-white font-bold rounded-lg shadow-xl flex justify-center p-4 row-span-2">
-                <CatAccord data={items} />
-            </div>
-            <div className="bg-yellow-500 text-white font-bold rounded-lg shadow-xl flex justify-center p-4 col-span-2">
-                <Themes />
-            </div>
-            <div className="bg-pink-500 text-white font-bold rounded-lg shadow-xl flex justify-center p-4 col-span-2 row-span-2">
-                <Themes />
-            </div>
-            <div className="bg-orange-500 text-white font-bold rounded-lg shadow-xl flex justify-center p-4 row-span-2">
-                <Themes />
-            </div>
-            <div className="bg-green-500 text-white font-bold rounded-lg shadow-xl flex justify-center p-4 col-span-2 row-span-2">
-                <Themes />
-            </div>
-            <div className="bg-cyan-500 text-white font-bold rounded-lg shadow-xl flex justify-center p-4">
-                <Themes />
+    return (
+        <div>
+            <div className="grid grid-rows-5 grid-cols-3 gap-4 grid-flow-row-dense pl-20 pr-20">
+                <div className="bg-red-500 text-white font-bold rounded-lg shadow-xl flex justify-center p-4 row-span-2">
+                    <CatAccord data={data}/>
+                </div>
+                <div className="bg-yellow-500 text-white font-bold rounded-lg shadow-xl flex justify-center p-4 col-span-2">
+                    <Themes />
+                </div>
+                <div className="bg-pink-500 text-white font-bold rounded-lg shadow-xl flex justify-center p-4 col-span-2 row-span-2">
+                    <Themes />
+                </div>
+                <div className="bg-orange-500 text-white font-bold rounded-lg shadow-xl flex justify-center p-4 row-span-2">
+                    <Themes />
+                </div>
+                <div className="bg-green-500 text-white font-bold rounded-lg shadow-xl flex justify-center p-4 col-span-2 row-span-2">
+                    <Themes />
+                </div>
+                <div className="bg-cyan-500 text-white font-bold rounded-lg shadow-xl flex justify-center p-4">
+                    <Themes />
+                </div>
             </div>
         </div>
-    </div>
-  );
+    );
 }
 
--- site/components/catInfoAccord.jsx
+++ site/components/catInfoAccord.jsx
@@ -5,67 +5,38 @@ import {
     AccordionTrigger,
 } from "@/components/ui/accordion"
 
-// export const CatAccord = () => {
-//     return (
-//         <div>
-//             <Accordion type="single" collapsible>
-//                 <AccordionItem value="faculty">
-//                     <AccordionTrigger>1 Faculty</AccordionTrigger>
-//                     <AccordionContent>
-//                         Jing
-//                     </AccordionContent>
-//                     <AccordionContent>
-//                         Jing
-//                     </AccordionContent>
-//                     <AccordionContent>
-//                         Jing
-//                     </AccordionContent>
-//                     <AccordionContent>
-//                         Jing
-//                     </AccordionContent>
-//                     <AccordionContent>
-//                         Jing
-//                     </AccordionContent>
-//                     <AccordionContent>
-//                         Jing
-//                     </AccordionContent>
-//                     <AccordionContent>
-//                         Jing
-//                     </AccordionContent>
-//                     <AccordionContent>
-//                         Jing
-//                     </AccordionContent>
-//                     <AccordionContent>
-//                         Jing
-//                     </AccordionContent>
-//                     <AccordionContent>
-//                         Jing
-//                     </AccordionContent>
-//                 </AccordionItem>
+export const CatAccord = ({ data }) => {
+    console.log(data);
+    return (
+        <div>
+            {Object.entries(data).map(([category, details]) => (
+            <div key={category}>
+                <h2>{category}</h2>
+            <Accordion type="single" collapsible>
+                <AccordionItem value="faculty">
+                    <AccordionTrigger>{details.faculty_count} Faculty</AccordionTrigger>
+                    <AccordionContent>
+                        
+                    </AccordionContent>
+                </AccordionItem>
 
-//                 <AccordionItem value="dept">
-//                     <AccordionTrigger>1 Department</AccordionTrigger>
-//                     <AccordionContent>
-//                         Computer science
-//                     </AccordionContent>
-//                 </AccordionItem>
+                <AccordionItem value="dept">
+                    <AccordionTrigger>1 Department</AccordionTrigger>
+                    <AccordionContent>
+                        Computer science
+                    </AccordionContent>
+                </AccordionItem>
 
-//                 <AccordionItem value="article">
-//                     <AccordionTrigger>1 Article</AccordionTrigger>
-//                     <AccordionContent>
-//                         Mouse Cheese
-//                     </AccordionContent>
-//                 </AccordionItem>
-//             </Accordion>
-
-//         </div>
-//     );
-// };
-
-export const CatAccord = (props) => {
-    console.log(props.Business)
-    return (<div>
-                <h1>{props.faculty_count}</h1>
+                <AccordionItem value="article">
+                    <AccordionTrigger>1 Article</AccordionTrigger>
+                    <AccordionContent>
+                        Mouse Cheese
+                    </AccordionContent>
+                </AccordionItem>
+            </Accordion>
             </div>
+            ))}
+        </div>
     );
 };
+
