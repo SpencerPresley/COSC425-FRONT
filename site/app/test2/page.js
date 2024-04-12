589360d317ada7dbd963e5006ec56b72ee8eff12
cbarbes <cbarbes02@gmail.com>
2024-04-12 15:17:04 -0400

    working on getting data to the site

--- site/app/test2/page.js
+++ site/app/test2/page.js
@@ -1,13 +1,29 @@
 import { Themes } from "@/components/Research"
 import { CatCard } from "@/components/catInfoCard"
 import { CatAccord } from "@/components/catInfoAccord"
+import fsPrommises from 'fs/promises';
+import path from 'path';
 
-export default function page() {
+
+export async function getStaticProps() {
+    // Path to the JSON file
+    const filePath = path.join(process.cwd(), 'public/test2.json');
+    
+    // Read the JSON file
+    const jsonData =await fsPrommises.readFile(filePath)
+    const data = JSON.parse(jsonData);
+  
+    // Pass the items array to the page via props
+    return objectData
+}
+
+  
+export default function page({items}) {
   return (
     <div>
         <div className="grid grid-rows-5 grid-cols-3 gap-4 grid-flow-row-dense pl-20 pr-20">
             <div className="bg-red-500 text-white font-bold rounded-lg shadow-xl flex justify-center p-4 row-span-2">
-                <CatAccord />
+                <CatAccord data={items} />
             </div>
             <div className="bg-yellow-500 text-white font-bold rounded-lg shadow-xl flex justify-center p-4 col-span-2">
                 <Themes />
@@ -28,3 +44,4 @@ export default function page() {
     </div>
   );
 }
+
--- site/components/catInfoAccord.jsx
+++ site/components/catInfoAccord.jsx
@@ -5,59 +5,67 @@ import {
     AccordionTrigger,
 } from "@/components/ui/accordion"
 
-export const CatAccord = () => {
-    return (
-        <div>
-            <Accordion type="single" collapsible>
-                <AccordionItem value="faculty">
-                    <AccordionTrigger>1 Faculty</AccordionTrigger>
-                    <AccordionContent>
-                        Jing
-                    </AccordionContent>
-                    <AccordionContent>
-                        Jing
-                    </AccordionContent>
-                    <AccordionContent>
-                        Jing
-                    </AccordionContent>
-                    <AccordionContent>
-                        Jing
-                    </AccordionContent>
-                    <AccordionContent>
-                        Jing
-                    </AccordionContent>
-                    <AccordionContent>
-                        Jing
-                    </AccordionContent>
-                    <AccordionContent>
-                        Jing
-                    </AccordionContent>
-                    <AccordionContent>
-                        Jing
-                    </AccordionContent>
-                    <AccordionContent>
-                        Jing
-                    </AccordionContent>
-                    <AccordionContent>
-                        Jing
-                    </AccordionContent>
-                </AccordionItem>
+// export const CatAccord = () => {
+//     return (
+//         <div>
+//             <Accordion type="single" collapsible>
+//                 <AccordionItem value="faculty">
+//                     <AccordionTrigger>1 Faculty</AccordionTrigger>
+//                     <AccordionContent>
+//                         Jing
+//                     </AccordionContent>
+//                     <AccordionContent>
+//                         Jing
+//                     </AccordionContent>
+//                     <AccordionContent>
+//                         Jing
+//                     </AccordionContent>
+//                     <AccordionContent>
+//                         Jing
+//                     </AccordionContent>
+//                     <AccordionContent>
+//                         Jing
+//                     </AccordionContent>
+//                     <AccordionContent>
+//                         Jing
+//                     </AccordionContent>
+//                     <AccordionContent>
+//                         Jing
+//                     </AccordionContent>
+//                     <AccordionContent>
+//                         Jing
+//                     </AccordionContent>
+//                     <AccordionContent>
+//                         Jing
+//                     </AccordionContent>
+//                     <AccordionContent>
+//                         Jing
+//                     </AccordionContent>
+//                 </AccordionItem>
 
-                <AccordionItem value="dept">
-                    <AccordionTrigger>1 Department</AccordionTrigger>
-                    <AccordionContent>
-                        Computer science
-                    </AccordionContent>
-                </AccordionItem>
+//                 <AccordionItem value="dept">
+//                     <AccordionTrigger>1 Department</AccordionTrigger>
+//                     <AccordionContent>
+//                         Computer science
+//                     </AccordionContent>
+//                 </AccordionItem>
 
-                <AccordionItem value="article">
-                    <AccordionTrigger>1 Article</AccordionTrigger>
-                    <AccordionContent>
-                        Mouse Cheese
-                    </AccordionContent>
-                </AccordionItem>
-            </Accordion>
+//                 <AccordionItem value="article">
+//                     <AccordionTrigger>1 Article</AccordionTrigger>
+//                     <AccordionContent>
+//                         Mouse Cheese
+//                     </AccordionContent>
+//                 </AccordionItem>
+//             </Accordion>
 
-        </div>
+//         </div>
+//     );
+// };
+
+export const CatAccord = (props) => {
+    console.log(props.Business)
+    return (<div>
+                <h1>{props.faculty_count}</h1>
+            </div>
     );
 };
