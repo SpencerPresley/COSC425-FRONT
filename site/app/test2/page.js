8f4e922911312d83ff928e73b96c2c4b75ed8328
Underlord711 <wchmar711@gmail.com>
2024-04-13 13:17:45 -0400

    Added the Key Funded Component

--- site/app/test2/page.js
+++ site/app/test2/page.js
@@ -3,13 +3,21 @@ import { CatCard } from "@/components/catInfoCard"
 import { CatAccord } from '@/components/catInfoAccord';
 import { LabSpace } from '@/components/labSpace'
 import { useCatData } from "@/components/useCatData.server";
+import { KeyFunded } from "@/components/keyFunded";
   
 export default function page() {
     const data = useCatData();
 
     return (
         <div>
-            <div className="grid grid-rows-5 grid-cols-3 gap-4 grid-flow-row-dense pl-20 pr-20">
+            {/* mt-10 = 10px margin top */}
+            {/* mx-10 = 10px margin left and right */}
+            {/* m-20 = 20px margin all around */}
+
+            {/* Standard practice is 12 columns and you divide them how you want so i changed that */}
+            {/* row-span-2 = take up 2 rows out of 5 */}
+            {/* col-span-2 = take up 2 columns out of 12*/}
+            <div className="grid grid-rows-5 grid-cols-3 gap-4 grid-flow-row-dense mt-10 mx-10 m-20">
                 <div className="bg-suMaroon text-white font-bold rounded-lg shadow-xl flex justify-center p-4 row-span-2">
                     <CatAccord data={data}/>
                 </div>
@@ -22,8 +30,8 @@ export default function page() {
                 <div className="bg-orange-500 text-white font-bold rounded-lg shadow-xl flex justify-center p-4 row-span-2">
                     <Themes />
                 </div>
-                <div className="bg-green-500 text-white font-bold rounded-lg shadow-xl flex justify-center p-4 col-span-2 row-span-2">
-                    <Themes />
+                <div className="bg-green-500 text-white rounded-lg shadow-xl p-4 col-span-2 row-span-2">
+                    <KeyFunded/>
                 </div>
                 <div className="bg-suMaroon rounded-lg shadow-xl flex justify-center p-4">
                     <LabSpace />
@@ -32,4 +40,3 @@ export default function page() {
         </div>
     );
 }
-
