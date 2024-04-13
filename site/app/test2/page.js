2465a60fb4eed8eb0ed582cd6c0f226f5dc21b43
Underlord711 <wchmar711@gmail.com>
2024-04-13 12:48:43 -0400

    changed the colors of completed components to suMaroon

--- site/app/test2/page.js
+++ site/app/test2/page.js
@@ -9,15 +9,8 @@ export default function page() {
 
     return (
         <div>
-            {/* mt-10 = 10px margin top */}
-            {/* mx-10 = 10px margin left and right */}
-            {/* m-20 = 20px margin all around */}
-
-            {/* Standard practice is 12 columns and you divide them how you want so i changed that */}
-            {/* row-span-2 = take up 2 rows out of 5 */}
-            {/* col-span-2 = take up 2 columns out of 12*/}
-            <div className="grid grid-rows-5 grid-cols-3 gap-4 grid-flow-row-dense mt-10 mx-10 m-20">
-                <div className="bg-red-500 text-white font-bold rounded-lg shadow-xl flex justify-center p-4 row-span-2">
+            <div className="grid grid-rows-5 grid-cols-3 gap-4 grid-flow-row-dense pl-20 pr-20">
+                <div className="bg-suMaroon text-white font-bold rounded-lg shadow-xl flex justify-center p-4 row-span-2">
                     <CatAccord data={data}/>
                 </div>
                 <div className="bg-yellow-500 text-white font-bold rounded-lg shadow-xl flex justify-center p-4 col-span-2">
@@ -32,7 +25,7 @@ export default function page() {
                 <div className="bg-green-500 text-white font-bold rounded-lg shadow-xl flex justify-center p-4 col-span-2 row-span-2">
                     <Themes />
                 </div>
-                <div className="bg-cyan-500 rounded-lg shadow-xl flex justify-center p-4">
+                <div className="bg-suMaroon rounded-lg shadow-xl flex justify-center p-4">
                     <LabSpace />
                 </div>
             </div>
