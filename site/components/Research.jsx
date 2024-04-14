17de8075d81e0206218207cd455e358c489b181a
Underlord711 <wchmar711@gmail.com>
2024-04-14 15:45:28 -0400

    Updated to pull data from Json

--- site/components/Research.jsx
+++ site/components/Research.jsx
@@ -4,13 +4,21 @@ export const Themes = ({ themedata }) => {
             <h1 className="font-bold">Research Topics</h1>
             {Object.entries(themedata).map(([theme, details]) => (
                 <div key={theme}>
-                    <ul>
-                        {Object.entries(details).map(([category, items]) => (
-                            <li key={category}>
-                                <h3>{category}</h3>
-                            </li>
-                        ))}
-                    </ul>
+                    <h2>{theme}</h2>
+                    <hr />
+                    {Object.entries(details).map(([category, items]) => (
+                        <div key={category}>
+                            <h3>{category}</h3>
+                            <ul>
+                                {Object.entries(items).map(([subCategory, subItems]) => (
+                                    <li key={subCategory}>
+                                        <h4>{subCategory}</h4>
+                                    </li>
+                                ))}
+                            </ul>
+                            <hr />
+                        </div>
+                    ))}
                 </div>
             ))}
         </div>
