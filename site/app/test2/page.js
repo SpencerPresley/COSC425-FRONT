17c52de5abff24e08100878c145dd8ab7f4fb24c
Underlord711 <wchmar711@gmail.com>
2024-04-14 15:29:16 -0400

    updated the page to bring in the useThemeData function

--- site/app/test2/page.js
+++ site/app/test2/page.js
@@ -4,9 +4,11 @@ import { CatAccord } from '@/components/catInfoAccord';
 import { LabSpace } from '@/components/labSpace'
 import { useCatData } from "@/components/useCatData.server";
 import { KeyFunded } from "@/components/keyFunded";
+import { useThemeData } from "@/components/useThemeData.server";
   
 export default function page() {
     const data = useCatData();
+    const themedata = useThemeData();
 
     return (
         <div>
@@ -22,13 +24,13 @@ export default function page() {
                     <CatAccord data={data}/>
                 </div>
                 <div className="bg-yellow-500 text-white font-bold rounded-lg shadow-xl flex justify-center p-4 col-span-2">
-                    <Themes />
+                    <LabSpace />
                 </div>
                 <div className="bg-pink-500 text-white font-bold rounded-lg shadow-xl flex justify-center p-4 col-span-2 row-span-2">
-                    <Themes />
+                    <LabSpace />
                 </div>
                 <div className="bg-orange-500 text-white font-bold rounded-lg shadow-xl flex justify-center p-4 row-span-2">
-                    <Themes />
+                    <Themes themedata={themedata} />
                 </div>
                 <div className="bg-suMaroon text-white rounded-lg shadow-xl p-4 col-span-2 row-span-2">
                     <KeyFunded/>
