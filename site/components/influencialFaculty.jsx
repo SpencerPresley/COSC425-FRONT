00abe0b79743ac68b91bc915b9064beadbb94cb6
Jude <judemaggitti123@gmail.com>
2024-04-14 06:42:33 -0400

    Updated the influencialFaculty to be a more proper list

--- site/components/influencialFaculty.jsx
+++ site/components/influencialFaculty.jsx
@@ -8,24 +8,25 @@ import {
 export const InfluencialFaculty = () => {
     return (
         <div>
-            <Accordion type="single" collapsible>
-                <AccordionItem value="faculty">
-                    <h2>Influencial Faculty:</h2>
-                    <li>
-                        <div>
+            <div value="faculty" className="text-white px-10 pb-10">
+                <h2>Influencial Faculty:</h2>
+                <div className="px-10">
+                    <div className="text-sm list-disk;">
+                        <br />
+                        <li>
                             Dr. Sally Smith, Computer science faculty<br />
-                            Member: 12 Publications, 2 Funding Awards
-                        </div>
-                        
-                    </li>
-                    <li>
-                        
+                            <p className="pl-5">Member: 12 Publications, 2 Funding Awards</p>
+                        </li>
+                    </div>
+                    <div className="text-sm list-disk">
+                        <br />
+                        <li>
                             Dr. Jeff Richards, Computer science faculty<br />
-                            Member: 7 Publications, 1 Funding Award
-                        
-                    </li>
-                </AccordionItem>
-            </Accordion>
+                            <p className="pl-5">Member: 7 Publications, 1 Funding Award</p>
+                        </li>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
