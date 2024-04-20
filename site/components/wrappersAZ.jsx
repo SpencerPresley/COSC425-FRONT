fcfdc91ed0e6f2e1ff1d46c554c63a1499942a3b
Jude <judemaggitti123@gmail.com>
2024-04-20 13:39:28 -0400

    Started development of categoryAZ page

---
+++ site/components/wrappersAZ.jsx
@@ -0,0 +1,26 @@
+export const Wrapper = ({ children }) => {
+
+    return (
+        <div className="bg-suMaroon flex flex-col sm:border-r sm:border-zinc-700">
+            {children}
+        </div>
+    );
+};
+
+export const PageWrapper = ({ children }) => {
+    return (
+      <div className="bg-suMaroon flex flex-col pt-10 px-10 space-y-2 bg-zinc-100 flex-grow pb-4">
+        {children}
+      </div>
+    );
+};
+
+export const OuterWrapper = ({ children }) => {
+  return (
+    <>
+      <div className="flex flex-col bg-suMaroon w-full h-3/6">
+          {children}
+      </div>
+    </>
+  );
+};
