846c67b69b1129de9d5d0315ddab336824456990
Jude <judemaggitti123@gmail.com>
2024-04-21 09:35:01 -0400

    begin updating so it can display all info

--- site/app/categoryAZ/[letter]/page.js
+++ site/app/categoryAZ/[letter]/page.js
@@ -11,8 +11,19 @@ export async function generateStaticParams() {
 async function getletterData(letter) {
   const data = await fetch("http://cosc425-category-data.s3.amazonaws.com/processed_category_data.json").then((res) => res.json());
 
-  // Find the letter object based on the 'url' property
+  // Find the category object based on the first letter in url property
   const filtEntries = Object.entries(data).filter(([_, item]) => item.url.startsWith(letter));
+
+  if (filtEntries) {
+    const letterData = filtEntries.map(([categoryName, entry]) => ({ categoryName, ...entry }));
+    return letterData;
+  }
+
+  return null;
+}
+
+async function getAll (letter) {
+  const data = await fetch("http://cosc425-category-data.s3.amazonaws.com/processed_category_data.json").then((res) => res.json());
 
   if (filtEntries) {
     const letterData = filtEntries.map(([categoryName, entry]) => ({ categoryName, ...entry }));
@@ -24,21 +35,29 @@ async function getletterData(letter) {
 
 export default async function Page({ params }) {
   const { letter } = params;
-  const letterData = await getletterData(letter);
+  letterData;
+
+  if(params=="all"||params==null)
+    letterData = await getletterData(letter);
 
   if (!letterData) {
     // Handle the case when letterData is undefined
     return <div>Letter not found</div>;
   }
 
+  const entrySets = [];
+  for(let i = 0; i < letterData.length; i += 4){
+    entrySets.push(letterData.slice(i, i+4));
+  }
+
   return (
     <div className="bg-suMaroon w-full h-full flex flex-col space-y-4">
       <span className="font-bold text-4xl text-center text-white flex flex-col justify-center">page action</span>
       <div className="overflow-y-auto">
-        <div className="max-h-screen flex flex-col space-y-2 m-8">
-          <div className="flex lg:flex-row flex-col shrink-0 w-full h-60 rounded-lg">
-            {letterData.map((entry, index) => (
-              <div key={index}>
+        {entrySets.map((set, setIndex) => (
+        <div key={setIndex} className="max-h-screen flex flex-col space-y-2 m-8">
+          <div className="flex lg:flex-row flex-col shrink- w-full h-60 rounded-lg">
+            {set.map((entry, index) => (
                 <CardAZ
                   title={entry.categoryName}
                   facultyCount={entry.faculty_count}
@@ -46,10 +65,10 @@ export default async function Page({ params }) {
                   articleCount={entry.article_count}
                   url={entry.url}
                 />
-              </div>
             ))}
           </div>
         </div>
+        ))}
       </div>
     </div>
   )
