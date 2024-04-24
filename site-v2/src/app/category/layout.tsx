dd2ccbebcf32751100b54a52c9bbd038766ef86a
SpencerPresley <spencerpresley96@gmail.com>
2024-04-23 22:37:25 -0400

    intial

---
+++ site-v2/src/app/category/layout.tsx
@@ -0,0 +1,19 @@
+import { SideNav } from "@/components/CategoryComponents/side-nav";
+
+export default function CategoryLayout({ children }: { children: React.ReactNode }) {
+  return (
+    
+      <div className="bg-black flex h-full w-full">
+        <div className="md:basis-1/4 pt-8">
+          <SideNav />
+        </div>
+        <div className="basis-4/4 md:basis-2/4 mt-5 mb-20">
+          {children}
+        </div>
+        <div className=" md:basis-1/4">
+          <p></p>
+        </div>
+      </div>
+  );
+}
+
