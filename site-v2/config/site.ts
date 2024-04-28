2e356fc29c9a9d9cd4f2885c0cc2cb15beb9b342
Underlord711 <wchmar711@gmail.com>
2024-04-28 10:42:32 -0400

    Updated Navbar to link to our github

--- site-v2/config/site.ts
+++ site-v2/config/site.ts
@@ -65,7 +65,7 @@ export const siteConfig = {
 
 	],
 	links: {
-		github: "https://github.com/nextui-org/nextui",
+		github: "https://github.com/SpencerPresley/COSC425-MAIN",
 		twitter: "https://twitter.com/getnextui",
 		docs: "https://nextui.org",
 		discord: "https://discord.gg/9b6yyZKmH4",
