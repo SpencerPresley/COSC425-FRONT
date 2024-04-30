01272b3625093072174802544b0954413b0124c4
SpencerPresley <SpencerPresley96@gmail.com>
2024-04-30 11:17:18 -0400

    changing site name and description metadata

--- site-v2/config/site.ts
+++ site-v2/config/site.ts
@@ -1,8 +1,8 @@
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-	name: "Next.js + NextUI",
-	description: "Make beautiful websites regardless of your design experience.",
+	name: "Rommel Center Initiative",
+	description: "AI Powered Research Database",
 	navItems: [
 		{
 			label: "Home",
